42b7747b49e487b5e804ce1ff5902537236f9cc2
Sofya <sofya.kirillova@saritasa.com>
2022-07-11 17:45:57 +0700

    fix #JC19-343

--- apps/vanilla/src/requests/verifyToken.ts
+++ apps/vanilla/src/requests/verifyToken.ts
@@ -7,7 +7,7 @@ export async function verifyToken(): Promise<boolean> {
   const accessToken = localStorage.getItem('access');
   let isVerifyToken = false;
   const errorCode = 401;
-  await api.post(`/auth/token/verify/`, {token: accessToken }).then(() => {
+  await api.post(`/auth/token/verify/`, { token: accessToken }).then(() => {
     isVerifyToken = true;
   })
     .catch(async res => {
